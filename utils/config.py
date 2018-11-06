e1a3608d3d2d69c23cb5b74af061b2b33639dbc9
Sebastian Otalora <juan.otaloramontenegro@hevs.ch>
2018-11-06 15:51:15 +0100

    Added config for hyperparam exploration

---
+++ utils/config.py
@@ -0,0 +1,3 @@
+PREFIX_IMGS = '/mnt/nas3/bigdatasets/Desuto/tupac_mitosis/mitoses_image_data_part_'
+PATH_GT = '/mnt/nas3/bigdatasets/Desuto/tupac_mitosis/mitoses_ground_truth/'
+#This file is used to define machine-dependent paths and shared variables across the differents scripts of the experiment
